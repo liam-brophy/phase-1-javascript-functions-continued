4827b726a21d5a17e5ab61f525cd334c1eb373c5
Liam Brophy <liambrophy.design@gmail.com>
2025-01-07 10:41:00 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+
+
+function mondayWork(monActivity = "go to the office"){
+    return `This Monday, I will ${monActivity}.`
+}
+
+
+function wrapAdjective(flare = "*"){
+    return function (compliment = "special"){
+        return `You are ${flare}${compliment}${flare}!`
+    }
+
+}
+
+wrapAdjective("%")("a dedicated programmer");
